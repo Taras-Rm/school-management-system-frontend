92ff6ff85d91f1528522c5de99aaeec9d46b2e2e
Taras Romaniuk <tarasrom25@gmail.com>
2023-09-10 13:51:03 +0300

    added possibility to create a school

--- src/api/school.js
+++ src/api/school.js
@@ -4,3 +4,8 @@ export const getAdminSchool = async () => {
   const response = await api.get(`/admins/school`);
   return response.data;
 };
+
+export const createSchool = async ({ name }) => {
+  const response = await api.post(`/admins/school`, { name });
+  return response.data;
+};
--- src/pages/admins/SchoolPage/SchoolPage.jsx
+++ src/pages/admins/SchoolPage/SchoolPage.jsx
@@ -1,10 +1,13 @@
-import { Button, Spin, Typography } from "antd";
+import { Button, Card, Col, Row, Spin, Typography } from "antd";
 import React, { useState } from "react";
 import { PlusCircleOutlined } from "@ant-design/icons";
 import { getAdminSchool } from "../../../api/school";
 import { useQuery } from "react-query";
+import CreateSchoolModal from "./components/CreateSchoolModal";
 
 function WithoutSchoolBoard() {
+  const [isCreateSchoolModalOpen, setIsCreateSchoolModalOpen] = useState(false);
+
   return (
     <div
       style={{
@@ -18,9 +21,18 @@ function WithoutSchoolBoard() {
       <Typography.Title level={2} style={{ marginBottom: 30 }}>
         If you want to use a system, first of all you should create a school
       </Typography.Title>
-      <Button size="large" type="primary" style={{ backgroundColor: "green" }}>
+      <Button
+        onClick={() => setIsCreateSchoolModalOpen(true)}
+        size="large"
+        type="primary"
+        style={{ backgroundColor: "green" }}
+      >
         Create a school <PlusCircleOutlined />
       </Button>
+      <CreateSchoolModal
+        isOpen={isCreateSchoolModalOpen}
+        setIsCreateSchoolModalOpen={setIsCreateSchoolModalOpen}
+      />
     </div>
   );
 }
@@ -39,8 +51,48 @@ function SchoolPage() {
   }
 
   return (
-    <div style={{ width: "100%" }}>
-      {school ? school.name : <WithoutSchoolBoard />}
+    <div
+      style={{
+        width: "100%",
+        backgroundColor: "rgb(215 215 215)",
+        padding: "10px 20px",
+      }}
+    >
+      {school ? (
+        <div>
+          <Typography.Title level={2}>{school.name} school</Typography.Title>
+          <div style={{ width: "100%" }}>
+            <Row gutter={16}>
+              <Col span={6}>
+                <Card
+                  title={
+                    <Typography.Text type="secondary">
+                      Teachers info
+                    </Typography.Text>
+                  }
+                >
+                  <Typography.Text style={{ fontSize: 30 }}>45</Typography.Text>
+                </Card>
+              </Col>
+              <Col span={6}>
+                <Card
+                  title={
+                    <Typography.Text type="secondary">
+                      Students info
+                    </Typography.Text>
+                  }
+                >
+                  <Typography.Text style={{ fontSize: 30 }}>
+                    435
+                  </Typography.Text>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      ) : (
+        <WithoutSchoolBoard />
+      )}
     </div>
   );
 }
