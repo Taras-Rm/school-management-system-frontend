df1af9e73ce6e5d0b01f32c60c1aa416e5b9961c
Taras Romaniuk <tarasrom25@gmail.com>
2023-10-02 13:58:33 +0300

    show number of stucents on class card

--- src/pages/admins/ClassesPage/components/ClassCard.jsx
+++ src/pages/admins/ClassesPage/components/ClassCard.jsx
@@ -1,10 +1,23 @@
-import { Card, Typography } from "antd";
+import { Card, Typography, message } from "antd";
 import React from "react";
 import { EditOutlined } from "@ant-design/icons";
 import { Link, generatePath } from "react-router-dom";
 import { routes } from "../../../routes";
+import { getSchoolClassStudents } from "../../../../api/classes";
+import { useQuery } from "react-query";
 
 function ClassCard({ classInfo }) {
+  const { data: students, isLoading: isLoadingStudents } = useQuery(
+    ["students", "class", classInfo.id],
+    () => getSchoolClassStudents({ id: classInfo.id }),
+    {
+      onError: (error) => {
+        message.error(error);
+      },
+      enabled: !!classInfo.id,
+    }
+  );
+
   return (
     <Card
       title={
@@ -21,6 +34,7 @@ function ClassCard({ classInfo }) {
         </Link>,
       ]}
       bordered={false}
+      loading={isLoadingStudents}
     >
       <div>
         <Typography.Text>Class teacher: </Typography.Text>
@@ -31,6 +45,10 @@ function ClassCard({ classInfo }) {
         ) : (
           <Typography.Text>not defined</Typography.Text>
         )}
+      </div>
+      <div>
+        <Typography.Text>Number of students: </Typography.Text>
+        <Typography.Text strong>{students?.length}</Typography.Text>
       </div>
     </Card>
   );
